d100f5b27e595a59e0c64847b3e0ef795b93a802
Luiz <luizrodrigo46@hotmail.com>
2017-08-09 13:22:30 -0300

    a template to display that everything worked

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
