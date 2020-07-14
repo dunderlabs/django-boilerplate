a74d18b2baef6b590875dfcd1e048d29c6f8ffa2
Patrick Mazulo <pmazulo@gmail.com>
2020-07-13 23:21:43 -0300

    Remodeling settings structure

--- settings/security.py
+++
@@ -1,8 +0,0 @@
-from .base import env
-
-
-# Key for salting hashes and such
-SECRET_KEY = env.str('SECRET_KEY', default='changeme')
-
-# Host names allowed when DEBUG=False
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['*'])
