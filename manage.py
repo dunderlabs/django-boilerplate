7986320890fd983a5371924cc6c8d88dba6d2351
Patrick Mazulo <pmazulo@gmail.com>
2020-07-13 23:22:06 -0300

    Get DJANGO_SETTINGS_MODULE from .env

--- manage.py
+++ manage.py
@@ -2,8 +2,16 @@
 import os
 import sys
 
+from environs import Env
+
+
+env = Env()
+env.read_env()  # read .env file, if it exists
+
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    settings_module = env.str('SETTINGS_MODULE', 'settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
     from django.core.management import execute_from_command_line
 
