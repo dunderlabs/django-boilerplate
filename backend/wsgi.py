9854841d5d35210d978e1913adef0f6fa216e491
Patrick Mazulo <pmazulo@gmail.com>
2021-06-22 00:36:58 -0300

    Fix references to settings.local

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -2,5 +2,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.local")
 application = get_wsgi_application()
